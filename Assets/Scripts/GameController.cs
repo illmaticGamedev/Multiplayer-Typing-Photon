6150a9d4f6fc5443473ba0a7b507ac21d846c99c
Shubham Rajanwar <actuallyshubham@gmail.com>
2021-11-23 05:22:13 +0530

    completedOfflineTypingMechanic

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -14,6 +14,12 @@ public class GameController : MonoBehaviour
     [SerializeField] private Slider mySliderScore;
     [SerializeField] private bool gameStarted = false;
     [SerializeField] private int currentWordCheckingIndex = 0;
+    [SerializeField] private TMP_Text highlightedWord;
+    
+    
+    [SerializeField] int initialWordCount;
+    [SerializeField] int wordsCompleted;
+    [SerializeField] private float currentPercentage;
     private void Start()
     {
         ChooseRandomParagraph();
@@ -24,21 +30,40 @@ public class GameController : MonoBehaviour
         int randomIndex = UnityEngine.Random.Range(0,wordsInCurrentParagraph.Count);
         textToType.text = paragraphs[randomIndex];
         string tempTextToTypeCopy = textToType.text;
-        
+        wordsCompleted = 0;
         ExtractWords(tempTextToTypeCopy);
+        UpdateSliderScore();
     }
 
 
     public void OnInputTextChange()
     {
+        RecheckHighlightedWord();
+        
         if (gameStarted)
         {
-            if (inputField.text == wordsInCurrentParagraph[currentWordCheckingIndex])
+            if (inputField.text== wordsInCurrentParagraph[currentWordCheckingIndex])
             {
                 wordsInCurrentParagraph.Remove(inputField.text);
+
+                if (textToType.text.Length == inputField.text.Length - 1)
+                {
+                    textToType.text = textToType.text.Remove(0,inputField.text.Length-1);
+                    
+                    currentPercentage = 100;
+                    mySliderScore.value = currentPercentage;
+                }
+                else
+                {
+                    textToType.text = textToType.text.Remove(0,inputField.text.Length);
+                }
+
                 inputField.text = "";
+                wordsCompleted += 1;
             }
         }
+
+        UpdateSliderScore();
     }
 
     void ExtractWords(string tempTextToTypeCopy)
@@ -52,15 +77,40 @@ public class GameController : MonoBehaviour
 
                 if (item == tempTextToTypeCopy[tempTextToTypeCopy.Length-1])
                 {
-                    wordsInCurrentParagraph.Add(currentWord);
+                    wordsInCurrentParagraph.Add(currentWord + " ");
                     currentWord = "";
                 }
             }
             else
             {
-                wordsInCurrentParagraph.Add(currentWord);
+                wordsInCurrentParagraph.Add(currentWord+" ");
                 currentWord = "";
             }
         }
+
+        initialWordCount = wordsInCurrentParagraph.Count;
+        RecheckHighlightedWord();
+    }
+
+
+    void RecheckHighlightedWord()
+    {
+        if (textToType.text == "")
+        {
+            highlightedWord.text = "";
+        }
+        else
+        {
+            highlightedWord.text = wordsInCurrentParagraph[0]; 
+        }
+    }
+
+    void UpdateSliderScore()
+    {
+        if (currentPercentage < 100)
+        {
+            currentPercentage  = (wordsCompleted * 100)/ initialWordCount;
+        }
+        mySliderScore.value = currentPercentage;
     }
 }
